c4fba4ef493fbe1baef5f6f5a52534e950ba3a57
Hwa Ryeong <hwaryeong1@gmail.com>
2020-10-29 19:11:27 -0600

    continue W6D3

--- W6D3/first_router_controller/app/controllers/users_controller.rb
+++ W6D3/first_router_controller/app/controllers/users_controller.rb
@@ -10,22 +10,42 @@ class UsersController < ApplicationController
     end
 
     def create #post
-        render json: params
+        # # user = User.find_by(id: params[:id])
+        # user = User.new(user_params)
+        
+        # if user.save
+        #     # user.update(user_params)
+        #     redirect_to user_url(user)
+        # else
+        #     render json: user.errors.full_message, status:422
+        # end
+        # user = User.new(params.require(:user).permit(:name, :email))
+        # # replace the `user_attributes_here` with the actual attribute keys
+        # user.save!
+        # render json: user
+        #   .require(:user)
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+          render json: user
+        else
+          render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def update #put/patch
-        # user = User.find_by(id: params[:id])
-        user = User.new(user_params)
-        
-        if user.save
-            # user.update(user_params)
-            redirect_to user_url(user)
+        user = User.find(params[:id])
+        if user.update_attributes(user_params)
+            render json: user
         else
-            render json: user.errors.full_message, status:422
+            render json: user.errors, status: :unprocessable_entity
         end
     end
-    
-    
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: user
+    end
     
     
     private 
